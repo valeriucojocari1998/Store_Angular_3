8bdb48677517c0bee9f7a19e0282cfff9ec538f8
Valeriu Cojocari <valeriu.cojocari@alvys.com>
2022-10-15 19:58:59 +0300

    6: Added footer and navbar basic

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,4 +1,11 @@
-import { dialogsComponents } from "./dialogs";
-import { wrappersComponents } from "./wrappers";
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { dialogsComponents } from './dialogs';
+import { wrappersComponents } from './wrappers';
 
-export const sharedComponents = [...wrappersComponents, ...dialogsComponents]
+export const sharedComponents = [
+  ...wrappersComponents,
+  ...dialogsComponents,
+  FooterComponent,
+  NavbarComponent,
+];
